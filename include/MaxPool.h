ca427e7d06de3292e432836b182ce6c847a5212a
ManoharSai2000 <45100887+ManoharSai2000@users.noreply.github.com>
2024-05-01 18:15:36 -0400

    include-cuda

---
+++ include/MaxPool.h
@@ -0,0 +1,31 @@
+//
+// Created by tabelini on 18/04/19.
+//
+
+#ifndef NEURAL_NET_IN_CPP_MAXPOOL_H
+#define NEURAL_NET_IN_CPP_MAXPOOL_H
+
+
+#include "Module.h"
+
+class MaxPool : public Module {
+private:
+
+public:
+    Tensor<double> output_;
+    Tensor<double> input_;
+    Tensor<int> indexes;
+    int stride_, size_;
+    explicit MaxPool(int size, int stride);
+
+    Tensor<double> &forward(Tensor<double> &input) override;
+
+    Tensor<double> backprop(Tensor<double> chainGradient, double learning_rate) override;
+
+    void load(FILE *file_model) override;
+
+    void save(FILE *file_model) override;
+};
+
+
+#endif //NEURAL_NET_IN_CPP_MAXPOOL_H
